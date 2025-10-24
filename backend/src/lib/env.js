4e55c4ca1edeb278e50c2d4779ad0b44c95e42b3
devansh <devansh020105@gmail.com>
2025-10-24 16:48:39 +0530

    welcome email created

---
+++ backend/src/lib/env.js
@@ -0,0 +1,26 @@
+//todo
+import "dotenv/config";
+
+export const ENV = {
+    PORT: process.env.PORT || 3000,
+    MONGO_URI:process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NODE_ENV : process.env.NODE_ENV,
+    CLIENT_URL: process.env.CLIENT_URL,
+    RESEND_API_KEY : process.env.RESEND_API_KEY,
+    EMAIL_FROM : process.env.EMAIL_FROM,
+    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+};
+
+// PORT = 3000
+// MONGO_URI = mongodb+srv://dailyzest2020_db_user:6YnMzlT5l4FKzqBI@cluster0.fz8gukb.mongodb.net/chatify_db?retryWrites=true&w=majority&appName=Cluster0
+// NODE_ENV = development
+
+// JWT_SECRET= myjwtsecret
+
+// RESEND_API_KEY = re_7XzXL3Vt_15fTEoyRsooch1977ZuQ198C
+
+// EMAIL_FROM = "onboarding@resend.dev"
+// EMAIL_FROM_NAME = "Devansh Sharma"
+
+// CLIENT_URL = http://localhost:5173
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,20 +1,21 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env";
 
 export const generateToken = (userId,res)=>{
-    const{JWT_SECRET,NODE_ENV} = process.env;
+    const{JWT_SECRET,NODE_ENV} = ENV;
     if(!JWT_SECRET){
-        throw new Error("JWT_SCCRET is not configured");
+        throw new Error("JWT_SECRET is not configured");
     }
     //create a token for a user
-    const token = jwt.sign({userid},process.env.JWT_SECRET,{
-        dexpiresIn: "7d",
+    const token = jwt.sign({userId}, JWT_SECRET,{
+        expiresIn: "15d",
     });
 
 res.cookie("jwt",token,{
     maxAge: 7*24*60*60*1000,
     httpOnly: true, // prevents XSS attacks: cross-site scripting
     sameSite: "strict", //CSRF attacks
-    secore: process.env.NODE_ENV === "development" ? false : true,
+    secure: ENV.NODE_ENV === "development" ? false : true,
 });
 
 return token;
