55526df5edef7904acadb4448c350f4d37e018fe
devansh <devansh020105@gmail.com>
2025-10-27 01:14:20 +0530

    login and logout implpemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -99,7 +99,7 @@ export const login = async (req,res) => {
 
 
 export const logout = async (_, res) => {
-    res.cookie("jwt", "",{maxAge: 0}) 
+    res.cookie("jwt", "",{maxAge: 0,httpOnly: true,}) 
     res.status(200).json({message: "Logged out successfully"})
 };
 
